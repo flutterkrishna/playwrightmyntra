1c86d8b5b4d7c2942ae075a78667c0bcb449e9b6
flutterkrishna <krishnakumarmrm@gmail.com>
2026-05-13 14:55:04 -0800

    confirmalert  added

--- tests/confirmAlert.spec.js
+++ tests/confirmAlert.spec.js
@@ -1,17 +1,36 @@
+import { test, webkit } from '@playwright/test';
 
+test.only('confirmAlert', async () => {
 
-test.only('confirmAlert', async ({ page }) => {
+    // launch browser
+    const browser = await webkit.launch({
+        headless: false
+    });
 
-    const browser= await webitkit.launch({
-        headless: true,
-        devtools: true
-    }); 
+    // create context
     const context = await browser.newContext();
+
+    // create page
     const page = await context.newPage();
 
-
+    // open url
     await page.goto('https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_alert');
-    await page.fill();
-    aw
 
+    // handle alert
+    page.on('dialog', async dialog => {
+
+        console.log(dialog.message());
+
+        await dialog.accept();
+    });
+
+    // switch to iframe
+    const frame = page.frameLocator('#iframeResult');
+
+    // click button
+    await frame.locator('button').click();
+
+    await page.waitForTimeout(3000);
+
+    await browser.close();
 });
