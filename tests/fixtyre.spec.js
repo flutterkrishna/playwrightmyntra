9cde8fd3815447f2f8fa8eaf1b13efb0f5a1c0de
flutterkrishna <krishnakumarmrm@gmail.com>
2026-05-13 14:51:54 -0800

    fixture added

--- tests/fixtyre.spec.js
+++ tests/fixtyre.spec.js
@@ -1,49 +1,50 @@
-//html dropdown site go
+import { test, chromium } from '@playwright/test';
 
-//testautomation site go
+test('handling dropdown and record video', async () => {
 
-test('handling dropdown and record video',async({})=>{
+    // launch browser
+    const browser = await chromium.launch({
+        headless: false
+    });
 
-//In built fixtures
-//browser
-//context
-//page
+    // create context
+    const context = await browser.newContext({
+        viewport: null,
 
-//use headless and viewport inbuilt fixtures to launch the browser in headed mode and set viewport to null
+        recordVideo: {
+            dir: 'records/'
+        }
+    });
 
+    // create page
+    const page = await context.newPage();
 
-//context is like a tab in the browser
-//page is like a page in the tab a window in the page
-//context screenshot folder create a folder with the name of the test and store the screenshots in that folder  
+    // demo dropdown website
+    await page.goto('https://testautomationpractice.blogspot.com/');
 
-const browser = await chromium.launch({headless:false,viewport:null});
-const context = await browser.newContext({
-    recordVideo:{
-        dir:'records/',
+    // select by value
+    await page.selectOption('#country', {
+        value: 'india'
+    });
 
-    }
+    await page.waitForTimeout(2000);
+
+    // select by label
+    await page.selectOption('#country', {
+        label: 'Japan'
+    });
+
+    await page.waitForTimeout(2000);
+
+    // select by index
+    await page.selectOption('#country', {
+        index: 3
+    });
+
+    await page.waitForTimeout(2000);
+
+    // close page/context/browser
+    await page.close();
+    await context.close();
+    await browser.close();
 });
-const  page = await context.newPage();
-
-//dropdown particular option select  is tagname select,div mostly
-//selectOptions()->handle dropdown
-await page.goto('https://www.ixigo.com/');
-
-await page.selectOption('#course',{value:'btech'});
-
-await page.waitForTimeout(5000);
-
-await page.selectOption('#course',{label:'B.Tech'});
-
-await page.waitForTimeout(5000);
-
-await page.selectOption('#course',{index:1});
-
-await page.waitForTimeout(5000);        
-
-await page.selectOption('#course',['ec','ij','vs']);
-await page.waitForTimeout(5000); 
-
-
-
-})
